95b611426049886ee6fc2f4a25905bbbec77b2a2
Kyle McKenna <mckennakyler@gmail.com>
2020-01-22 18:36:00 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(pharse = "Squawk!")
+def parrot
+  pharse = "Squawk!"
   puts pharse
+  return pharse
 end  
 parrot("Pretty bird!")
